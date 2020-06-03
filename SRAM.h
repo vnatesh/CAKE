9ecb3bc6cd06924d690ab541a9f2f0300363b0a9
vnatesh <vikas.natesh@gmail.com>
2020-06-02 20:04:02 -0500

    SRAM receiving weight and act packets from DRAM, re-assembles block and sends to maestro

--- SRAM.h
+++ SRAM.h
@@ -4,105 +4,160 @@
 #include "PacketSwitch.h"
 
 
-SC_MODULE (SRAM) {
+SC_MODULE(SRAM) {
+
+
+  Connections::In<PacketSwitch::Packet> sram_dram_in;
+  Connections::Out<PacketSwitch::Packet> sram_dram_out;
 
   Connections::In<PacketSwitch::Packet> packet_in;
   Connections::Out<PacketSwitch::Packet> packet_out;
-  sc_in <bool> clk;
-  sc_in <bool> rst;
 
-  vector<vector<PacketSwitch::AccumType>> weights;
-  vector<vector<PacketSwitch::AccumType>> activations;
+  sc_in_clk     clk;
+  sc_in<bool> rst;
 
-  PacketSwitch::ID_type id;
 
-  SC_CTOR(SRAM) {
+  // 0-3 is MB ind, 4-7 is SA ind, 8 is SRAM, 9 is CB
+  SC_HAS_PROCESS(SRAM);
+  SRAM(sc_module_name name_) : sc_module(name_) {
     SC_THREAD(run);
     sensitive << clk.pos();
-    async_reset_signal_is(rst, false);
+    NVHLS_NEG_RESET_SIGNAL_IS(rst);
   }
 
 
   void run() {
 
-    PacketSwitch::Packet   p_in;
-    PacketSwitch::Packet   p_out1;
-    PacketSwitch::Packet   p_out2;
-
+    sram_dram_in.Reset();
+    sram_dram_out.Reset();
     packet_out.Reset();
     packet_in.Reset();
-    // Wait for initial reset.
     wait(20.0, SC_NS);
 
-    // send in weights then data
-    int M = weights.size(); // 8 when tile_sz = 2
-    int K = weights[0].size(); // 8 when tile_sz = 2
-    int N = activations[0].size(); // 16 when tile_sz = 2 
+    PacketSwitch::Packet p_in;
+    PacketSwitch::Packet p_in2;
 
-    // Send weights to SA, loop over each weight tile and send it to corresponding MB
-    printf("Sending weights from SRAM to MB \n");
-    for (int m = 0; m < M/tile_sz; m++) { // row
-      for (int k = 0; k < K/tile_sz; k++) { // col
+    PacketSwitch::Packet p_out1;
+    PacketSwitch::Packet p_out2;
 
-        for (int i = 0; i < tile_sz; i++) {
-          for (int j = 0; j < tile_sz; j++) {
-            p_out1.data[i][j] = weights[m * tile_sz + i][k * tile_sz + j];
-          }
-        }
+    int m_cnt = 0; // weight row
+    int k_cnt = 0; // weight col / act row
+    int n_cnt = 0; // act col
 
-        wait(5);
+    // int M = weight_block.size(); // 8 when tile_sz = 2
+    // int K = weight_block[0].size(); // 8 when tile_sz = 2
+    // int N = activation_block[0].size(); // 16 when tile_sz = 2 
 
-        p_out1.src = 999; // sram src
-        p_out1.srcPod = 0; // sram default src pod
-        p_out1.dst = k;
-        p_out1.dstPod = m;
-        p_out1.d_type = 0;
-        packet_out.Push(p_out1);     
-        wait(10);
-      }
-    }
+    int M = Wy;
+    int K = Wz;
+    int N = Dx;
 
-    // TODO : this is a triple loop due to the broadcast. Change packet header to include BCAST instruction
-    printf("Sending activations from SRAM to MB \n");
-    for (int n = 0; n < N/tile_sz; n++){
-      cout << "Sending vector " << n << "\n";
-      for (int k = 0; k < K/tile_sz; k++) {
-        for (int m = 0; m < M/ tile_sz; m++) {
-     
+    vector<vector<PacketSwitch::AccumType>> weight_block(M, vector<PacketSwitch::AccumType>(K)); 
+    vector<vector<PacketSwitch::AccumType>> activation_block(K, vector<PacketSwitch::AccumType>(N)); 
+
+    while(1) {
+      if(sram_dram_in.PopNB(p_in)) {
+
+        if(p_in.d_type == 0) {
+
           for (int i = 0; i < tile_sz; i++) {
             for (int j = 0; j < tile_sz; j++) {
-              p_out2.data[i][j] = activations[k * tile_sz + i][n * tile_sz + j];
+              weight_block[m_cnt * tile_sz + i][k_cnt * tile_sz + j] = p_in.data[i][j];
             }
           }
 
           wait(5);
 
-          p_out2.src = 999;
-          p_out2.srcPod = 0;
-          p_out2.dst = k;
-          p_out2.dstPod = m;
-          p_out2.d_type = 1;
-          packet_out.Push(p_out2);
-          wait(10);
-        }
-      }
-    }
-
-
-    while(1) {
-
-      if(packet_in.PopNB(p_in)) {
-
-        for (int i = 0; i < tile_sz; i++) {
-          for (int j = 0; j < tile_sz; j++) {
-            printf("%d ", p_in.data[i][j]);
+          k_cnt++;
+          if(k_cnt ==  K / tile_sz) {
+            k_cnt = 0;
+            m_cnt++;
           }
-          printf("\n");
-        }
-        printf("\n");
-      }
 
-      wait(5);
+          if(m_cnt == M / tile_sz) {
+
+            printf("Sending weight block from SRAM to Maestro \n");
+            for (int m = 0; m < M/tile_sz; m++) { // row
+              for (int k = 0; k < K/tile_sz; k++) { // col
+
+                for (int i = 0; i < tile_sz; i++) {
+                  for (int j = 0; j < tile_sz; j++) {
+                    p_out1.data[i][j] = weight_block[m * tile_sz + i][k * tile_sz + j];
+                  }
+                }
+
+                wait(5);
+
+                p_out1.src = 999; // sram src
+                p_out1.srcPod = 0; // sram default src pod
+                p_out1.dst = k;
+                p_out1.dstPod = m;
+                p_out1.d_type = 0;
+                packet_out.Push(p_out1); 
+
+                wait(10);
+              }
+            }
+
+            m_cnt = 0;
+          }
+        } 
+
+        else if(p_in.d_type == 1) {
+
+          for (int i = 0; i < tile_sz; i++) {
+            for (int j = 0; j < tile_sz; j++) {
+              activation_block[k_cnt * tile_sz + i][n_cnt * tile_sz + j] = p_in.data[i][j];
+            }
+          }
+
+          wait(5);
+
+          k_cnt++;
+          if(k_cnt ==  K / tile_sz) {
+            k_cnt = 0;
+            n_cnt++;
+          }
+
+          if(n_cnt ==  N / tile_sz) {
+
+            // TODO : this is a triple loop due to the broadcast. Change packet header to include BCAST instruction
+            printf("Sending activation block from SRAM to Maestro \n");
+            for (int n = 0; n < N/tile_sz; n++){
+              for (int k = 0; k < K/tile_sz; k++) {
+                for (int m = 0; m < M/ tile_sz; m++) {
+             
+                  for (int i = 0; i < tile_sz; i++) {
+                    for (int j = 0; j < tile_sz; j++) {
+                      p_out2.data[i][j] = activation_block[k * tile_sz + i][n * tile_sz + j];
+                    }
+                  }
+
+                  wait(5);
+
+                  p_out2.src = 999;
+                  p_out2.srcPod = 0;
+                  p_out2.dst = k;
+                  p_out2.dstPod = m;
+                  p_out2.d_type = 1;
+                  packet_out.Push(p_out2);
+
+                  wait(10);
+                }
+              }
+            }
+
+            n_cnt = 0;
+          }
+        }
+      }
+      
+
+      if(packet_in.PopNB(p_in2)) {
+        sram_dram_out.Push(p_in2);
+      } 
+
+      wait();            
     }
   }
 };
