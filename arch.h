968f05b228c3f46987762a40bccf13d847cdfdcf
vnatesh <vikas.natesh@gmail.com>
2020-07-23 23:37:53 -0500

    updated notation and new parameters

--- arch.h
+++ arch.h
@@ -1,91 +1,57 @@
 #ifndef __ARCH_H__
 #define __ARCH_H__
-
 #include <systemc.h>
 #include <nvhls_int.h>
 #include <nvhls_connections.h>
 #include <nvhls_vector.h>
-
 #include <ArbitratedScratchpad.h>
 #include <ArbitratedScratchpad/ArbitratedScratchpadTypes.h>
-
 #include <sys/time.h>
 #include<bits/stdc++.h> 
 
 const static bool DEBUG = false;
-
-
-/*
-
-Let's assume the number of blocks is very big.
-
-Given the number of systolic arrays and the dram bandwidth, figure out how
-much SRAM is needed to keep everything busy.
-
-plot curve, x axis is the DRAM bandwidth and
-y axis the minimum SRAM size to have high usage. For some number of
-systolic arrays, we have a plot/ a line. Then add a sa... add another line.
-Show how more SAs need more SRAM and relation to DRAM and SRAM sizes. Try and
-get the function. Later on we can come up with the math. Not terribly important
-yet.
-
-Pod grows with systolic, with a factor of 4.
-
-
-notes from 6/8/2020
-Blocking and waiting, hard to know deterministic without running it.
-HT says its deterministic but is a waste of time.
-DRAM may hog the switch, so its not available.
-
-Measure by checking everything going out of SA, know how many computations you have done.
-Put some counters on the sender side, if receiver only receives 90 then you know theres a block
-Now we have to figure out where the counters should go.
-
-HT says make some assumptions to ensure that the program runs.
-
-*/
-
-
-/* examples:
-    P = 2:    2*2 = 4 pods, each pod has 2x2 SAs    (16 SAs)
-    P = 4:    4*4 = 16 pods, each pod has 4x4 SAs   (256 SAs)
-    P = 8:    8*8 = 64  pods, each pod has 8x8 SAs  (4096 SAs)
-*/
-
-// Define P, each pod has (P,P) element pairs
-// Number of processing elements is Wz* Wy
-// const static int P = 4;
-const static int P = 2;
-const static int alpha = 2;
 const static int tile_sz = 2; // change to 8 later
-const static int NUM_PODS = P*P;
-const static int POD_SZ = P*P;
-const static int NUM_CB = 1;
+
+const static double R = 1.5; // DRAM banwidth 
+const static int alpha = (int) (1 / (R-1));
+
+// number of SAs on H-tree (square 2d array of SAs)
+const static int Sx = 4;
+const static int Sy = 4;
+const static int NUM_LEVELS = (int) ceil(log(((double) Sx*Sy)) / log(2));
+
+// shape of a pod sx x sy. For now, only 2x2 pods supported
+const static int sx = 2;
+const static int sy = 2;
+const static int POD_SZ = sx * sy;
+const static int NUM_PODS = (int) ((Sx*Sy) / POD_SZ); // user can opt to use only a portion of the total
+const static int NUM_AB = 1;
 const static int NUM_SRAM = 1;
 
-const static int Dx = alpha*P*P*tile_sz; // Size of data block in N dimension i.e alpha*Wz
-const static int Dz = P*P*tile_sz; // Size of data block in the k dimension
-const static int Wz = P*P*tile_sz; // Size of block in the K dimension, same as Dz
-const static int Wy = Wz; // Size of block in the M dimension
+// An operation block is sized/shaped to a pod and available DRAM bw
+const static int N_ob = alpha; // Size of operation block in N dimension in terms of tiles
+const static int K_ob = sx; // Size of operation block in the k dimension in terms of tiles
+const static int M_ob = sy; // Size of block in the M dimension in terms of tiles
+
+// user can choose the shape of the SRAM block. The N_sr dim is always N_ob
+// eg. 3x5 OBs vs 4x4 vs 2x6 etc
+// In order to have BW_sr ≤ BW_dw when we quadruple #SAs by using 4 pods, we must
+// increase SRAM block size by 4x in M and N dim. This shape is used below
+// const static int M_sr = NUM_PODS * M_ob; 
+// const static int K_sr = K_ob;
+// const static int N_sr = NUM_PODS * N_ob;
 
 
-// size of SRAM block
-const static int M_sr = Wy;
-const static int K_sr = Wz;
-const static int N_sr = Dx;
-// const static int M_sr = Wy*4; // for P = 4
-// const static int K_sr = Wz*4;
-// const static int N_sr = Dx*4;
+const static int M_sr = 2*M_ob; 
+const static int K_sr = 2*K_ob;
+const static int N_sr = 2*N_ob;
 
-// size of DRAM block
-const static int M_dr = M_sr*3;
-const static int K_dr = K_sr*3;
-const static int N_dr = N_sr*3;
+// the number of pods the user actually uses is based on the SRAM block shape
+const static int NUM_PODS_USED = (int) ((M_sr/M_ob) * (K_sr/K_ob)); 
 
-// size of full matrices to be multiplied in the current layer
-const static int M = M_dr*2;
-const static int K = K_dr*2;
-const static int N = N_dr*2;
 
+// Full MM block is a 3d collection of SRAM blocks
+const static int M = M_sr;
+const static int K = K_sr;
+const static int N = N_sr;
 #endif
-
