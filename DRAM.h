f2937e0dfff7816f71243819d65ce2647478723d
vnatesh <vikas.natesh@gmail.com>
2020-07-23 23:36:55 -0500

    updated notation

--- DRAM.h
+++ DRAM.h
@@ -48,44 +48,44 @@ SC_MODULE(DRAM) {
     sc_time start, end;
     start = sc_time_stamp();
 
-    for (int n1 = 0; n1 < (N / N_dr); n1++) {
+    for (int n1 = 0; n1 < (N / N_sr); n1++) {
 
-      for (int m_prime = 0; m_prime < (M / M_dr); m_prime++) {
+      for (int m_prime = 0; m_prime < (M / M_sr); m_prime++) {
 
         int m1;
 
         if((n1 % 2) == 0) {
           m1 = m_prime;
         } else {
-          m1 = (M / M_dr) - m_prime - 1;
+          m1 = (M / M_sr) - m_prime - 1;
         }
 
-        for (int k_prime = 0; k_prime < (K / K_dr); k_prime++) {
+        for (int k_prime = 0; k_prime < (K / K_sr); k_prime++) {
 
           int k1;
 
           if ((m_prime % 2) == 0) {
             k1 = k_prime;
           } else {
-            k1 = (K / K_dr) - k_prime - 1;
+            k1 = (K / K_sr) - k_prime - 1;
           }
 
           if(DEBUG) cout <<  n1 << " " << m1 << " " << k1 << "\n";
 
           // Send weights to SRAM. For each block, loop over the weight tiles, assign src/dst addrs
           if(DEBUG) cout << "Sending weights from DRAM to SRAM \n";
-          for (int m = 0; m < M_dr/tile_sz; m++) { // row
-            for (int k = 0; k < K_dr/tile_sz; k++) { // col
+          for (int m = 0; m < M_sr; m++) { // row
+            for (int k = 0; k < K_sr; k++) { // col
 
               for (int i = 0; i < tile_sz; i++) {
                 for (int j = 0; j < tile_sz; j++) {
-                  p_out1.data[i][j] = weights[(m1 * M_dr) + (m * tile_sz) + i][(k1 * K_dr) + (k * tile_sz) + j];
+                  p_out1.data[i][j] = weights[(m1 * M_sr * tile_sz) + (m * tile_sz) + i][(k1 * K_sr * tile_sz) + (k * tile_sz) + j];
                 }
               }
 
-              p_out1.X = (m1 * M_dr/tile_sz) + (m);
+              p_out1.X = (m1 * M_sr) + m;
               p_out1.Y = -1;
-              p_out1.Z = (k1 * K_dr/tile_sz) + (k);
+              p_out1.Z = (k1 * K_sr) + k;
 
               // wait(10);
               wait();
@@ -99,18 +99,18 @@ SC_MODULE(DRAM) {
           }
 
           if(DEBUG) cout << "Sending activations from DRAM to SRAM \n";
-          for (int n = 0; n < N_dr/tile_sz; n++) {
-            for (int k = 0; k < K_dr/tile_sz; k++) {
+          for (int n = 0; n < N_sr; n++) {
+            for (int k = 0; k < K_sr; k++) {
            
               for (int i = 0; i < tile_sz; i++) {
                 for (int j = 0; j < tile_sz; j++) {
-                  p_out2.data[i][j] = activations[(k1 * K_dr) + (k * tile_sz) + i][(n1 * N_dr) + (n * tile_sz) + j];
+                  p_out2.data[i][j] = activations[(k1 * K_sr * tile_sz) + (k * tile_sz) + i][(n1 * N_sr * tile_sz) + (n * tile_sz) + j];
                 }
               }
 
               p_out2.X = -1;
-              p_out2.Y = (n1 * N_dr/tile_sz) + (n);
-              p_out2.Z = (k1 * K_dr/tile_sz) + (k);
+              p_out2.Y = (n1 * N_sr) + n;
+              p_out2.Z = (k1 * K_sr) + k;
 
               // wait(10);
               wait();
@@ -159,7 +159,7 @@ SC_MODULE(DRAM) {
 
         p_cnt++;
 
-        if(p_cnt == ((M / tile_sz) * (N / tile_sz))) {
+        if(p_cnt == (M * N)) {
           end = sc_time_stamp();
           io_time_recv += (end - start).to_default_time_units();
           cout << "\n\nMAESTRO MMM RESULT\n\n";
