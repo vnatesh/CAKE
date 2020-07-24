96ef752e1f960eb77da7c7b8acb1552818570f39
vnatesh <vikas.natesh@gmail.com>
2020-07-23 23:35:11 -0500

    instantiate nodes of H-tree

--- maestro.h
+++ maestro.h
@@ -2,42 +2,43 @@
 #define __MAESTRO_H__
 
 #include "PacketSwitch.h"
-#include "MB.h"
+#include "LeafSwitch.h"
+#include "SB.h"
 #include "SA.h"
-#include "CB.h"
+#include "AB.h"
 
 SC_MODULE(Maestro) {
 
-  PacketSwitch* p_switch;
-  MB* mb[NUM_PODS][POD_SZ];
-  SA* sa[NUM_PODS][POD_SZ];
-  CB* cb[NUM_PODS];
+  PacketSwitch* p_switch[Sx*Sy - 1];
+  AB* ab[Sx*Sy - 1];
 
-
+  LeafSwitch* leaf_switch[Sx*Sy];
+  SB* sb[Sx*Sy];
+  SA* sa[Sx*Sy];
+  
   Connections::In<PacketSwitch::Packet> maestro_sram_in;
   Connections::Out<PacketSwitch::Packet> maestro_sram_out;
 
   Connections::In<PacketSwitch::Packet> packet_in;
   Connections::Out<PacketSwitch::Packet> packet_out;
 
-  sc_in_clk     clk;
+  sc_in_clk   clk;
   sc_in<bool> rst;
-
 
   // 0-3 is MB ind, 4-7 is SA ind, 8 is SRAM, 9 is CB
   SC_HAS_PROCESS(Maestro);
   Maestro(sc_module_name name_) : sc_module(name_) {
 
-    p_switch = new PacketSwitch(sc_gen_unique_name("p_switch"));
-
-    for (int j = 0; j < NUM_PODS; j++) {
-      for (int i = 0; i < POD_SZ; i++) {
-        mb[j][i] = new MB(sc_gen_unique_name("mb"));
-        sa[j][i] = new SA(sc_gen_unique_name("sa"));
-      }
-      cb[j] = new CB(sc_gen_unique_name("cb"));
+    for(int i = 0; i < Sx*Sy - 1; i++) {
+      p_switch[i] = new PacketSwitch(sc_gen_unique_name("p_switch"));
+      // ab[i] = new AB(sc_gen_unique_name("ab"));
     }
 
+    for (int i = 0; i < Sx*Sy; i++) {
+      leaf_switch[i] = new LeafSwitch(sc_gen_unique_name("leaf_switch"));
+      sb[i] = new SB(sc_gen_unique_name("sb"));
+      sa[i] = new SA(sc_gen_unique_name("sa"));      
+    }
 
     SC_THREAD(send_packet);
     sensitive << clk.pos();
@@ -49,7 +50,7 @@ SC_MODULE(Maestro) {
 
   }
 
-
+  // recv from SRAM, send to maestro
   void send_packet() {
 
     maestro_sram_in.Reset();
@@ -67,7 +68,7 @@ SC_MODULE(Maestro) {
     }
   }
 
-
+  // receive from maesrtro, send to SRAM
   void receive_packet() {
 
     maestro_sram_out.Reset();
