a1a14432d8c088ae9fa5c4dce8a0077343a6653e
vnatesh <vikas.natesh@gmail.com>
2021-03-29 20:15:06 -0400

    updated experiments

--- experiments.py
+++ experiments.py
@@ -59,6 +59,7 @@ static char *WEIGHT_FILE = "%s";
 static char *DATA_FILE = "%s";
 static char *RESULT_FILE = "%s";
 static const char delims[] = " ";
+const static char int_bw_growth = '%s'; // constant vs increasing     
 
 const static bool DEBUG = 0;
 const static bool LOG = 0;
@@ -75,7 +76,7 @@ const static int Sy = %d;
 const static int NUM_SA = Sx * Sy;
 const static int NUM_LEVELS = (int) ceil(log(((double) NUM_SA)) / log(2));
 const static int POD_SZ = sx * sy;
-const static int NUM_PODS = (int) (NUM_SA / POD_SZ); // user can opt to use only a portion of the total
+const static int NUM_PODS = %d; //(int) (NUM_SA / POD_SZ); // user can opt to use only a portion of the total
 const static int pod_ab_hops = (int) ceil(log(((double) sx)) / log(2));
 
 const static int M_ob = sy; // Size of block in the M dimension in terms of tiles
@@ -91,9 +92,9 @@ const static int M = %d;
 const static int K = %d;
 const static int N = %d;
 #endif
-''' % (c['perf_file'], c['w_file'], c['d_file'], c['r_file'], c['tile_sz'], 
+''' % (c['perf_file'], c['w_file'], c['d_file'], c['r_file'], c['int_bw_growth'], c['tile_sz'], 
   c['alpha'], c['lat_ext'], c['lat_int'], c['s'], c['s'], c['Sx'], 
-  c['Sy'], 'NUM_PODS', '1', c['M'], c['K'], c['N'])
+  c['Sy'], c['NUM_PODS'], 'NUM_PODS', '1', c['M'], c['K'], c['N'])
   
 
 
@@ -126,38 +127,38 @@ def mat_dim_test(fname = "mat_dim_test"):
 
 
 
-def cpu_comp_test(fname = "cpu_comp_test"):
-  CAKE_params = {'M' : 1024, 'K' : 1024, 'N' : 1024,
-      'Sx' : None, 'Sy' : None, 's' : 8, 'R' : 2, 
-      'tile_sz' : 16, 'lat_ext' : 1, 'lat_int' : 1, 
-      'w_file' : 'weights', 'd_file' : 'data', 
-      'r_file' : 'result', 'perf_file' : fname}
-  #
-  dims = [(8,8),(16,8),(16,16),(32,16)]
-  exec_num = 3742;
-  f = open(fname, 'w')
-  f.write("Sx,Sy,number of cycles\n")
-  f.close()
-  #
-  for i in dims:
-    CAKE_params['Sx'] = i[0]
-    CAKE_params['Sy'] = i[1]
-    arch = build_arch(CAKE_params)
-    print(arch)
-    run_exp(arch, exec_num)       
-    exec_num += 1
+# def cpu_comp_test(fname = "cpu_comp_test"):
+#   CAKE_params = {'M' : 1024, 'K' : 1024, 'N' : 1024,
+#       'Sx' : None, 'Sy' : None, 's' : 8, 'R' : 2, 
+#       'tile_sz' : 16, 'lat_ext' : 1, 'lat_int' : 1, 
+#       'w_file' : 'weights', 'd_file' : 'data', 
+#       'r_file' : 'result', 'perf_file' : fname}
+#   #
+#   dims = [(8,8),(16,8),(16,16),(32,16)]
+#   exec_num = 3742;
+#   f = open(fname, 'w')
+#   f.write("Sx,Sy,number of cycles\n")
+#   f.close()
+#   #
+#   for i in dims:
+#     CAKE_params['Sx'] = i[0]
+#     CAKE_params['Sy'] = i[1]
+#     arch = build_arch(CAKE_params)
+#     print(arch)
+#     run_exp(arch, exec_num)       
+#     exec_num += 1
 
 
 
 def cpu_comp_test(fname = "cpu_comp_test3"):
   CAKE_params = {'M' : 256, 'K' : 256, 'N' : 256,
-      'Sx' : None, 'Sy' : None, 's' : None, 'alpha' : None, 
+      'Sx' : None, 'Sy' : None, 's' : None, 'NUM_PODS' : None, 'alpha' : None, 
       'tile_sz' : 16, 'lat_ext' : 1, 'lat_int' : 1, 
       'w_file' : 'weights', 'd_file' : 'data', 
-      'r_file' : 'result', 'perf_file' : fname}
+      'r_file' : 'result', 'int_bw_growth' : None, 'perf_file' : fname}
   #
   # dims = [(8,8),(16,8),(16,16),(32,16)]
-  dims = [(2,2,2,1).(4,2,2,1),(4,4,4,2),(8,4,4,2),(8,8,8,4),(16,8,8,4),(16,16,16,8)]
+  dims = [(2,2,2,1),(4,2,2,1),(4,4,4,2),(8,4,4,2),(8,8,8,4),(16,8,8,4),(16,16,16,8)]
   exec_num = 777;
   f = open(fname, 'w')
   f.write("Sx,Sy,s,alpha,number of cycles\n")
@@ -176,157 +177,68 @@ def cpu_comp_test(fname = "cpu_comp_test3"):
 
 
 def exp1fig1(fname = "exp1fig1"):
-  arch1 = include + '''
-const std::string PERF_FILE = "%s"; 
-const static double lat_internal_factor = 0; 
-const static int tile_sz = 2; // change to 8 later
-const static double R = 2; // DRAM banwidth 
-const static int alpha = (int) (1 / (R-1));
-const static int lat_dram = 4; // DRAM latency 
-
-''' % fname
-#
-  arch2 = '''
-const static int NUM_SA = Sx * Sy;
-const static int NUM_LEVELS = (int) ceil(log(((double) NUM_SA)) / log(2));
-const static int sx = 2;
-const static int sy = 2;
-const static int POD_SZ = sx * sy;
-const static int NUM_PODS = (int) (NUM_SA / POD_SZ); // user can opt to use only a portion of the total
-const static int pod_ab_hops = (int) ceil(log(((double) sx)) / log(2));
-
-const static int M_ob = sy; // Size of block in the M dimension in terms of tiles
-const static int K_ob = sx; // Size of operation block in the k dimension in terms of tiles
-const static int N_ob = sx * alpha * NUM_PODS; // Size of operation block in N dimension in terms of tiles
-
-'''
-#
-  M=32
-  K=32
-  N=64
-  # increase by NUM_PODS in the M and N dims
-  # test_dims = [ [(2, 2), ('NUM_PODS', '1')], 
-  # 			       [(4, 2), ('NUM_PODS', '1')], 
-  #               [(4, 4), ('NUM_PODS', '1')], 
-  #               [(8, 4), ('NUM_PODS', '1')], 
-  #               [(8, 8), ('NUM_PODS', '1')]]
-  test_dims = [ [(4, 4), ('NUM_PODS', '1')], 
-                [(8, 4), ('NUM_PODS', '1')],
-                [(8, 8), ('NUM_PODS', '1')],
-                [(16, 8), ('NUM_PODS', '1')]]
-  # increasing internal bw linearly with # SAs
-  # lin_inc = [16,8,4,2,1]
-  lin_inc = [16,8,4,2]
-  # lin_inc = [8,5,3,2,1]
-  # constant internal bw even though # SAs is increasing linearly
-  constant = [16,16,16,16]
-  # constant = [16,16,16]
-  # constant = [24,12,6,3]
-  # constant = [5,5,5,5,5]
-  # lat_internals = [lin_inc] + [constant]
-  lat_internals = [constant]
-  # bw_growth = ['I','C']
-  bw_growth = ['C']
+  CAKE_params = {'M' : 96, 'K' : 96, 'N' : 96,
+      'Sx' : 8, 'Sy' : 8, 's' : 2, 'NUM_PODS' : None, 'alpha' : 1, 
+      'tile_sz' : 2, 'lat_ext' : 4, 'lat_int' : 1, 
+      'w_file' : 'weights', 'd_file' : 'data', 
+      'r_file' : 'result', 'int_bw_growth' : None, 'perf_file' : fname}
+  # NUM_PODS = [1,2,3,4,6,8,12,16]
+  # POD_SZ = [(2,2),(4,2),(4,4),(4,4),(8,4),(8,4),(8,8),(8,8)]
+  # bw_growth = {'I' : [1,1,1,1,1,1,1,1], 'C' : [1,2,3,4,6,8,12,16]}
   #
+  # NUM_PODS = [1,2,3,4]
+  # POD_SZ = [(4,4),(8,4),(8,8),(8,8)]
+  # bw_growth = {'I' : [4,4,3,2]}
+  #
+#   [128.0, 64.0, 42.666666666666664, 32.0, 25.6, 21.333333333333332, 18.285714285714285, 16.0, 14.222222222222221, 12.8, 11.636363636363637, 10.666666666666666, 9.846153846153847, 9.142857142857142, 8.533333333333333, 8.0]
+# [32.0, 16.0, 10.666666666666666, 8.0, 6.4, 5.333333333333333, 4.571428571428571, 4.0, 3.5555555555555554, 3.2, 2.909090909090909, 2.6666666666666665, 2.4615384615384617, 2.2857142857142856, 2.1333333333333333, 2.0]
+  NUM_PODS = [1,2,3,4,6,8,12,16]
+  # POD_SZ = [(2,2),(4,2),(4,4),(4,4),(8,4),(8,4),(8,8),(8,8)]
+  bw_growth = {'I' : [16,16,11,8,5,4,3,2], 'C' : [16,16,16,16,16,16,16,16]}
   f = open(fname, 'w')
-  f.write("number of SAs,number of cycles,bw growth\n")
+  f.write("number of pods,number of cycles,bw growth\n")
   f.close()
   exec_num = 200;
-  #
-  for l in xrange(len(bw_growth)):
-    for i in xrange(len(test_dims)):
-      a1 = '''
-const static char bw_growth = '%s'; // constant vs increasing     
-const static int lat_internal = %d; // internal latency 
-const static int Sx = %d;
-const static int Sy = %d;
-''' % (bw_growth[l], lat_internals[l][i], test_dims[i][0][0], test_dims[i][0][1])
-      a3 = '''
-const static int M_sr = M_ob * %s; 
-const static int K_sr = K_ob * %s;
-''' % (test_dims[i][1][0], test_dims[i][1][1])
-      a4 = '''
-const static int N_sr = N_ob;
-const static int NUM_PODS_USED = (int) ((M_sr/M_ob) * (K_sr/K_ob)); 
-const static int M = %d;
-const static int K = %d;
-const static int N = %d;
-#endif
-''' % (M,K,N)
-      arch = arch1 + a1 + arch2 + a3 + a4
-      run_exp(arch, exec_num)  	    
+  for k in bw_growth:
+    for i in range(len(NUM_PODS)):
+      CAKE_params['NUM_PODS'] = NUM_PODS[i]
+      # CAKE_params['Sx'] = POD_SZ[i][0]
+      # CAKE_params['Sy'] = POD_SZ[i][1]
+      CAKE_params['int_bw_growth'] = k
+      CAKE_params['lat_int'] = bw_growth[k][i]
+      arch = build_arch(CAKE_params)
+      # print(arch)
+      run_exp(arch, exec_num)       
       exec_num += 1
-
 
 
 
 def exp1fig2(fname = "exp1fig2"):
-  arch1 = include + '''
-const std::string PERF_FILE = "%s"; 
-const static char bw_growth = 'N'; // constant vs increasing     
-const static double lat_internal_factor = 0; 
-const static int tile_sz = 2; // change to 8 later
-const static double R = 2; // DRAM banwidth 
-const static int alpha = (int) (1 / (R-1));
-''' % fname
-  #
-  arch2 = '''
-const static int NUM_SA = Sx * Sy;
-const static int NUM_LEVELS = (int) ceil(log(((double) NUM_SA)) / log(2));
-const static int sx = 2;
-const static int sy = 2;
-const static int POD_SZ = sx * sy;
-const static int NUM_PODS = (int) (NUM_SA / POD_SZ); // user can opt to use only a portion of the total
-const static int pod_ab_hops = (int) ceil(log(((double) sx)) / log(2));
-
-const static int M_ob = sy; // Size of block in the M dimension in terms of tiles
-const static int K_ob = sx; // Size of operation block in the k dimension in terms of tiles
-const static int N_ob = sx * alpha * NUM_PODS; // Size of operation block in N dimension in terms of tiles
-'''
-  #
-  M=32
-  K=32
-  N=32
-  test_dims = [ [(2, 2), ('NUM_PODS', '1')], 
-             [(4, 2), ('NUM_PODS', '1')], 
-              [(4, 4), ('NUM_PODS', '1')], 
-              [(8, 4), ('NUM_PODS', '1')], 
-              [(8, 8), ('NUM_PODS', '1')]]
-  lat_internal = [16,8,4,2,1]
-  sx=2
-  sy=2
-  #
+  CAKE_params = {'M' : 96, 'K' : 96, 'N' : 96,
+      'Sx' : 8, 'Sy' : 8, 's' : 2, 'NUM_PODS' : None, 'alpha' : 1, 
+      'tile_sz' : 2, 'lat_ext' : 1, 'lat_int' : 1, 
+      'w_file' : 'weights', 'd_file' : 'data', 
+      'r_file' : 'result', 'int_bw_growth' : 'I', 'perf_file' : fname}
+#
+  NUM_PODS = [1,2,3,4,6,8,12,16]
+  # POD_SZ = [(2,2),(4,2),(4,4),(4,4),(8,4),(8,4),(8,8),(8,8)]
+  bw_growth = [16,16,11,8,5,4,3,2]
+  lat_ext = [32,16,8,4]
   f = open(fname, 'w')
-  f.write("number of SAs,number of cycles,K\n")
+  f.write("number of pods,number of cycles,lat_dram\n")
   f.close()
-  exec_num = 100;
-  #
-  for factor in range(1,5):
-    for i in xrange(len(test_dims)):
-      a1 = '''
-const static int lat_dram = 1; // DRAM latency 
-const static int lat_internal = %d; // internal latency 
-const static int Sx = %d;
-const static int Sy = %d;
-''' % (lat_internal[i], test_dims[i][0][0], test_dims[i][0][1])
-      a3 = '''
-//const static int lat_link[NUM_LEVELS] = NULL;
-const static int M_sr = M_ob * %s; 
-const static int K_sr = K_ob * %s;
-''' % (test_dims[i][1][0], test_dims[i][1][1])
-      a4 = '''
-const static int N_sr = N_ob;
-const static int NUM_PODS_USED = (int) ((M_sr/M_ob) * (K_sr/K_ob)); 
-const static int M = %d;
-const static int K = %d;
-const static int N = %d;
-#endif
-''' % (M , K * factor, N)
-      arch = arch1 + a1 + arch2 + a3 + a4
+  exec_num = 300;
+  for j in range(len(lat_ext)):
+    for i in range(len(NUM_PODS)):
+      CAKE_params['NUM_PODS'] = NUM_PODS[i]
+      # CAKE_params['Sx'] = POD_SZ[i][0]
+      # CAKE_params['Sy'] = POD_SZ[i][1]
+      CAKE_params['lat_int'] = bw_growth[i]
+      CAKE_params['lat_ext'] = lat_ext[j]
+      arch = build_arch(CAKE_params)
+      # print(arch)
       run_exp(arch, exec_num)       
       exec_num += 1
-    # print arch
-
 
 
 
@@ -814,11 +726,11 @@ const static int N = %d;
 
 
 if __name__ == '__main__':
-  cpu_comp_test()
+  # cpu_comp_test()
   # mat_dim_test()
   # test()
-  # exp1fig1()
-  # exp1fig2()
+  exp1fig1()
+  exp1fig2()
   # exp1fig3()
   # exp1fig4()
   # exp1fig5()
